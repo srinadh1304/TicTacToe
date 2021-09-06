2f8166e09b317d4aae69e8fda2f43c75a628041a
srinadh1304 <srinadh1304@gmail.com>
2021-09-06 08:20:39 +0530

    ADDED: Created a board with character array

--- src/com/bridgelabz/tictactoe/TicTacToeMain.java
+++ src/com/bridgelabz/tictactoe/TicTacToeMain.java
@@ -5,5 +5,11 @@ public class TicTacToeMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome to tic tac toe program");
 	}
-
+	public void Board() {
+		char boardArray[]=new char[11];
+		for(int size=1;size<boardArray.length;size++)
+		{
+			boardArray[size]='\0';
+		}
+	}
 }
