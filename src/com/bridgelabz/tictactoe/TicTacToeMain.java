086ab9c314ea1dceb2c16f3548d0c64103be0afb
srinadh1304 <srinadh1304@gmail.com>
2021-09-06 08:25:24 +0530

    REFACTORED: Given proper size for board

--- src/com/bridgelabz/tictactoe/TicTacToeMain.java
+++ src/com/bridgelabz/tictactoe/TicTacToeMain.java
@@ -1,15 +1,15 @@
 package com.bridgelabz.tictactoe;
 
 public class TicTacToeMain {
-
+	char boardArray[]=new char[10];
 	public static void main(String[] args) {
 		System.out.println("Welcome to tic tac toe program");
 	}
 	public void Board() {
-		char boardArray[]=new char[11];
+		
 		for(int size=1;size<boardArray.length;size++)
 		{
-			boardArray[size]='\0';
+			boardArray[size]=' ';
 		}
 	}
 }
